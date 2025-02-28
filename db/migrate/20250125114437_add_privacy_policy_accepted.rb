e77c60a86a58da1bf365087cddf8bb7ea710419b
Raphael Taxis <raphael.taxis@fu-berlin.de>
2025-02-28 11:22:59 +0100

    set default value of privacy_policy_accepted to false
    - only during migration

--- db/migrate/20250125114437_add_privacy_policy_accepted.rb
+++ db/migrate/20250125114437_add_privacy_policy_accepted.rb
@@ -1,8 +1,12 @@
 class AddPrivacyPolicyAccepted < ActiveRecord::Migration[6.1]
   def change
-    add_column :offers, :privacy_policy_accepted, :boolean, null: false
-    add_column :requests, :privacy_policy_accepted, :boolean, null: false
-    add_column :answers, :privacy_policy_accepted, :boolean, null: false
-    add_column :subscriptions, :privacy_policy_accepted, :boolean, null: false
+    add_column :offers, :privacy_policy_accepted, :boolean, null: false, default: false
+    add_column :requests, :privacy_policy_accepted, :boolean, null: false, default: false
+    add_column :answers, :privacy_policy_accepted, :boolean, null: false, default: false
+    add_column :subscriptions, :privacy_policy_accepted, :boolean, null: false, default: false
+    change_column_default :offers, :privacy_policy_accepted, from: false, to: nil
+    change_column_default :requests, :privacy_policy_accepted, from: false, to: nil
+    change_column_default :answers, :privacy_policy_accepted, from: false, to: nil
+    change_column_default :subscriptions, :privacy_policy_accepted, from: false, to: nil
   end
 end
